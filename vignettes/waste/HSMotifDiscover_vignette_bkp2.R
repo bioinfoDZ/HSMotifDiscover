dd93da4fdf222002b9ca4c6f594b9e27f59bc86f
bioinfoDZ <zhengLab353>
2021-03-24 01:32:44 +0100

    remove clutter

--- vignettes/waste/HSMotifDiscover_vignette_bkp2.R
+++
@@ -1,7 +0,0 @@
-## ----setup, include = FALSE---------------------------------------------------
-knitr::opts_chunk$set(collapse = TRUE,comment = "#>")
-
-## ----gh-installation, eval = FALSE--------------------------------------------
-#  # install.packages("devtools")
-#  devtools::install_github("bioinfoDZ/HSMotifDiscover",build_vignettes = FALSE )
-
